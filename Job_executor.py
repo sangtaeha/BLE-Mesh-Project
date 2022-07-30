4a4676e5a950ba7ef4eeaec9e20f100c6fbe3841
Sai Akhil <matsy@users.noreply.github.com>
2022-07-30 15:13:27 -0600

    End to end functionality is complete

--- Job_executor.py
+++ Job_executor.py
@@ -3,44 +3,79 @@ import pymongo
 from datetime import datetime
 import json
 from bson import ObjectId
+import subprocess
 
 client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 db = client.get_database('total_records')
 
 fmt = '%Y-%m-%dT%H:%M'
+home_dir = '/home/matsy007/Downloads/Mesh/BLE-Mesh-Project/Commands/'
 
 # remove from queue and add to executed jobs
 def getJobs():
     time_now = datetime.now().strftime(fmt)
     log_data = []
-    # prune jobs that are completed from the queue
-    cmds_search = db.queue_jobs.find({"execution_time":{'$lt':time_now}})
-    print(cmds_search)
-    for job in cmds_search:
-        cmd_id = job["_id"]
-        job_id = job["_id"]
-        exec_job_input = {'_id': cmd_id, 'execution_time': time_now}
-        db.queue_jobs.delete_one({"_id":job_id})
-        log_data.append("Removing this job from the queue list")
-        db.executed.insert_one(exec_job_input)
-        log_data.append("Adding this executed job from the queue list")
-        # TO DO: delete the command*.txt files too
 
     # complete jobs that are to be done now
     cmds_search = db.queue_jobs.find({"execution_time":time_now})
     print(cmds_search)
     for job in cmds_search:
-        #print(json.dumps(job, indent=4))
-        #log_data.append(json.dumps(job, indent=4))
+        # get the command details and chip_info
         cmd_id = job["_id"]
+        cmds_search = list(db.commands.find({"_id":job["_id"]}))
+        cmd = cmds_search[0]
+        # turn the entire group on or off
+        if cmd['chip_id'] is "" :
+            chips_search = list(db.chip_info.find({"Group_ID":cmd["group_id"]}))
+            app_key = chips_search[0]["Group_ID"]
+            address_handle = chips_search[0]["Group_ID_handle"]
+        else:
+            chips_search = list(db.chip_info.find({"Chip_ID":cmd["chip_id"]}))
+            app_key = chips_search[0]["Group_ID"]
+            address_handle = chips_search[0]["Address_handle"]
+
+        # log_data.append(json.dumps(job, indent=4))        
         job_id = job["_id"]
         exec_job_input = {'_id': cmd_id, 'execution_time': time_now}
         db.queue_jobs.delete_one({"_id":job_id})
         log_data.append("Removing this job from the queue list")
         db.executed.insert_one(exec_job_input)
         log_data.append("Adding this executed job from the queue list")
+
+        # TO DO: generate the commands file here: 
+        # doing it here so that even if commands if I gen file here => no race condition
+        file_name = home_dir+'cmd_'+str(job_id)+'.txt'
+        with open(file_name,"a") as cmd_file:
+            cmd_file.write('db = MeshDB("database/example_database.json")\n')
+            cmd_file.write('db.nodes\n')
+            cmd_file.write('gc = GenericOnOffClient()\n')
+            cmd_file.write('device.model_add(gc)\n')
+            # check if you have to light entire group or chip alone
+            cmd_file.write('gc.publish_set({0}, {1})\n'.format(app_key, address_handle))
+            if cmd["cmd_val"]==1:
+                cmd_file.write('gc.set(True)\n')
+            else:
+                cmd_file.write('gc.set(False)\n')
+
         # TO DO: run the interactive python shell script system command
+        print("Running the system command: "+file_name)
+        subprocess.run(["python3", "interactive_pyaci.py","-d", "COM8", "-l","3" ,"<",file_name])
+        
         # TO DO: delete the command*.txt files too
+        #subprocess.run(["rm","-rf",file_name])
+        
+        # prune jobs that are completed from the queue
+        cmds_search = db.queue_jobs.find({"execution_time":{'$lt':time_now}})
+        print(cmds_search)
+        for job in cmds_search:
+            cmd_id = job["_id"]
+            job_id = job["_id"]
+            exec_job_input = {'_id': cmd_id, 'execution_time': time_now}
+            db.queue_jobs.delete_one({"_id":job_id})
+            log_data.append("Removing this job from the queue list")
+            db.executed.insert_one(exec_job_input)
+            log_data.append("Adding this executed job from the queue list")
+            # TO DO: delete the command*.txt files too
 
 
     for log in log_data:
@@ -55,4 +90,4 @@ if __name__ == '__main__':
         print("##########################################################################################")
         getJobs()
         print("##########################################################################################")
-        sleep(58)
+        sleep(30)
--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import pymongo, bcrypt, json, queue
 from datetime import datetime
 from datetime import timedelta
 from bson import ObjectId
+import subprocess, signal
 
 app = Flask(__name__)
 app.secret_key = "testing"
@@ -17,6 +18,8 @@ records = db.register
 
 # for formatting time delta
 fmt = '%Y-%m-%dT%H:%M'
+home_dir = '/home/matsy007/Downloads/Mesh/BLE-Mesh-Project/Commands/'
+
 def delta_to_string(duration):
     days, seconds = duration.days, duration.seconds
     weeks = max(days // 7, 0)
@@ -44,7 +47,7 @@ def delta_to_string(duration):
     elif seconds>0:
         return "{} secs".format(seconds), "bi bi-circle-fill activity-badge text-danger align-self-start"
     else:
-        return ""
+        return "", ""
 
 @app.route('/sign_out')
 def sign_out(user=""):
@@ -237,6 +240,44 @@ def bluetooth_devices():
         print(user)
     return render_template('bluetooth-devices.html', user=user)
 
+# Code for running the job immediately
+def run_command(cmd_id):
+    cmds_search = list(db.commands.find({"_id":cmd_id}))
+    print(cmd_id)
+    cmd = cmds_search[0]
+    print(cmd)
+
+    # turn the entire group on or off
+    if cmd['chip_id'] is "" :
+        chips_search = list(db.chip_info.find({"Group_ID":cmd["group_id"]}))
+        app_key = chips_search[0]["Group_ID"]
+        address_handle = chips_search[0]["Group_ID_handle"]
+    else:
+        chips_search = list(db.chip_info.find({"Chip_ID":cmd["chip_id"]}))
+        app_key = chips_search[0]["Group_ID"]
+        address_handle = chips_search[0]["Address_handle"]
+
+    # TO DO: create the file    
+    file_name = home_dir+'cmd_'+str(cmd_id)+'.txt'
+    with open(file_name,"a") as cmd_file:
+        cmd_file.write('db = MeshDB("database/example_database.json")\n')
+        cmd_file.write('db.nodes\n')
+        cmd_file.write('gc = GenericOnOffClient()\n')
+        cmd_file.write('device.model_add(gc)\n')
+        # check if you have to light entire group or chip alone
+        cmd_file.write('gc.publish_set({0}, {1})\n'.format(app_key, address_handle))
+        if cmd["cmd_val"]==1:
+            cmd_file.write('gc.set(True)\n')
+        else:
+            cmd_file.write('gc.set(False)\n')
+
+    # TO DO: run the interactive python shell script system command
+    print("Running the system command: "+file_name)
+    subprocess.run(["python3", "interactive_pyaci.py","-d", "COM8", "-l","3" ,"<",file_name])
+    
+    # TO DO: delete the command*.txt files too
+    #subprocess.run(["rm","-rf",file_name])
+
 # TO DO: While scheduling the jobs itself create the commands.txt file
 @app.route('/scheduled_jobs', methods=['post','get'])
 def scheduled_jobs():
@@ -278,6 +319,9 @@ def scheduled_jobs():
                 db.queue_jobs.insert_one(job_input)
             else:
                 log_data.append("Scheduling Immediately")
+                print("Scheduling Immediately")
+                #TO DO: run_command()
+                run_command(cmd_id)
                 db.executed.insert_one(job_input)
         
         # this is scheduling periodic intervals
@@ -298,6 +342,9 @@ def scheduled_jobs():
                     db.queue_jobs.insert_one(job_input)
                 else:
                     log_data.append("Scheduling Immediately")
+                    print("Scheduling Immediately")
+                    # TO DO: run_command()
+                    run_command(cmd_id)
                     db.executed.insert_one(job_input)
                 
                 # update the time
@@ -335,7 +382,7 @@ def scheduled_jobs():
     for job in jobsqueue:
         print(job)
         job_exec = {}
-        if time_curr > job["execution_time"]:
+        if time_curr >= job["execution_time"]:
             continue
         job_exec["time"],job_exec["font"] = delta_to_string(datetime.strptime(job["execution_time"], fmt) - datetime.now())
         log_data.append('\nJob with ID: \n'.format(str(job["_id"])))
@@ -389,12 +436,11 @@ def command(cmd_id):
         updated_cmd["chip_id"] = chip_id
         updated_cmd["group_id"] = group_id
         updated_cmd["cmd_val"] = cmd_val
-        updated_job["cmd_id"] = cmd_id
         updated_job["execution_time"] = time_now
 
         # update the db: queue(update) and commands(replace)
         db.commands.replace_one({"_id":ObjectId(cmd_id)}, updated_cmd, upsert=True)
-        db.queue_jobs.replace_one({"cmd_id":cmd_id}, updated_job, upsert=True)
+        db.queue_jobs.replace_one({"_id":ObjectId(cmd_id)}, updated_job, upsert=True)
 
         return redirect(url_for('commands'))
 
@@ -431,7 +477,7 @@ def commands():
         main_command["chip_id"] = command["chip_id"]
         main_command["group_id"] = command["group_id"]
         main_command["cmd_val"] = command["cmd_val"]
-        main_command["_id"] = str(command["_id"])
+        main_command["cmd_id"] = str(command["_id"])
         duration = datetime.strptime(job["execution_time"], fmt)-datetime.now()
         print(duration)
         main_command["time"], main_command["font"] = delta_to_string(duration)
@@ -472,4 +518,12 @@ def icons_boxicons():
    return render_template('icons-boxicons.html')
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", debug = True)
+    # running the python job_executor script endlessly
+    #subprocess.Popen(
+    #    ['/usr/bin/python3','/home/matsy007/Downloads/Mesh/BLE-Mesh-Project/Job_executor.py'],
+    #    stdin=subprocess.DEVNULL,
+    #    stdout = open('job_logs.out','a'),
+    #    stderr = subprocess.STDOUT,
+    #    start_new_session=True
+    #)
+    app.run(host="0.0.0.0", debug = True)
